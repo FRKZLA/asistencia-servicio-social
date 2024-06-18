a5251f980899451acfcdcdc573b09308aa31b5c8
Hector Mtz <hec.gre81@gmail.com>
2024-06-17 19:12:42 -0600

    Add navigation bar

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,4 +1,22 @@
+'use client'
+
+import Link from "next/link"
+import styles from './navbar.module.css'
+import { usePathname } from 'next/navigation'
+
 const NavBar = () => {
+    const pathname = usePathname()
+
+    return (
+        <main className={styles.container}>
+            <Link href="/" className={styles.item + ` ${pathname === '/' ? styles.active : ''}`}>
+                Entrada y Salida
+            </Link>
+            <Link href="/alumnos" className={styles.item + ` ${pathname === '/alumnos' ? styles.active : ''}`}>
+                Lista de Alumnos
+            </Link>
+        </main>
+    )
 
 }
 
