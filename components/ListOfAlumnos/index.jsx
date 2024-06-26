f9811ebef739e0ad5cb81188a4ea9327407b2e78
Hector Mtz <hec.gre81@gmail.com>
2024-06-25 19:10:28 -0600

    Add loading message

--- components/ListOfAlumnos/index.jsx
+++ components/ListOfAlumnos/index.jsx
@@ -7,18 +7,21 @@ import styles from './list.module.css'
 
 const ListOfAlumnos = () => {
   const [alumnos, setAlumnos] = useState([])
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
+    setLoading(true)
     getUsuarios()
       .then((response) => {
         console.log(response)
         setAlumnos(response)
+        setLoading(false)
       })
-
   }, [])
 
 
   return (
     <article className={styles.flex_container}>
+      {loading && <p className={styles.center}>Cargando...</p>}
       {alumnos.map((alumno) => (
         <Link href={`/alumnos/${alumno.id}`} key={alumno.id} className={styles.item}>
           <h3># {alumno.id}</h3>
