0f55c9d878e972419347039c0b89579d93bf6605
Paburo <p.ivan.a.puente@gmail.com>
2024-07-25 15:49:41 -0600

    ROLL BACK HOLY SHUT

--- app/alumnos/page.jsx
+++ app/alumnos/page.jsx
@@ -1,13 +1,6 @@
 import FloatingButtonComponent from '@/components/FloatingButton'
 import ListOfAlumnos from '@/components/ListOfAlumnos'
 import pageStyles from '../page.module.css'
-import '../globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
 
 const ListOfAlumnosPage = () => {
   return (
