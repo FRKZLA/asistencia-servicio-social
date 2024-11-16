e342f05b703d0e4f26cd6a7e401c8991c830b899
Hector Mtz <hec.gre81@gmail.com>
2024-11-15 20:34:54 -0600

    Add required state for date

--- app/add_asistencias/page.jsx
+++ app/add_asistencias/page.jsx
@@ -6,6 +6,7 @@ import ListOfAlumnos from '@/components/ListOfAlumnos';
 import { useState } from 'react';
 
 import styles from './page.module.css'
+import Button from '@/components/Button';
 
 const AddAlumnosPage = () => {
   const [day, setDay] = useState('');
@@ -19,21 +20,23 @@ const AddAlumnosPage = () => {
           min='2024-06-01'
           max='2024-06-30'
           title='Fecha'
+          required
           onChange={(e) => setDay(e.target.value)}
         />
+        <ListOfAlumnos lateral>
+          <article className={styles.inputs}>
+            <Input
+              type="time"
+              title="Inicio"
+            />
+            <Input
+              type="time"
+              title="Fin"
+            />
+          </article>
+        </ListOfAlumnos>
+        <Button>Guardar</Button>
       </Form>
-      <ListOfAlumnos lateral>
-        <article className={styles.inputs}>
-          <Input
-            type="time"
-            title="Inicio"
-          />
-          <Input
-            type="time"
-            title="Fin"
-          />
-        </article>
-      </ListOfAlumnos>
     </main>
   )
 
